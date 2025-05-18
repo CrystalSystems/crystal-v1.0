a2083741e78d832c625b09c720664c1cb9f1ee22
Andrew <andrew.shedov@gmail.com>
2025-05-18 19:33:44 +0300

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,41 +9,46 @@ import {
   hashtagRoutes,
   likedRoutes
 } from "./routes/index.js";
-import bodyParser from "body-parser";
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 import { PRODUCTION_STATUS } from "./constants/index.js";
+
 const app = express();
+
 // application mode
 app.use(
   cors({
     origin: true,
     credentials: !PRODUCTION_STATUS,
-  }))
+  })
+);
 // /application mode
-// cookie
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cookieParser())
-// /cookie 
+
+// parsers
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+// /parsers
+
 app.listen(port, (error) => {
   error ?
     console.log("Server error -", error) :
     console.log("Server is running");
 });
+
 app.use(
   postRoutes,
   userRoutes,
   hashtagRoutes,
-  likedRoutes);
+  likedRoutes
+);
+
 // express.static
 app.use("/uploads/", express.static("uploads/"));
 // /express.static
+
 // database Connection
 mongoose
-  .connect(
-    "mongodb://127.0.0.1:27017/crystal"
-  )
+  .connect("mongodb://127.0.0.1:27017/crystal")
   .then(() => console.log("DB connected"))
   .catch((error) => console.log("DB error -", error));
 // /database Connection
