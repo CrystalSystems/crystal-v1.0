bdbe9946848081777149d9a1bf5a84e456769456
Andrew <andrew.shedov@gmail.com>
2025-07-03 01:18:51 +0300

    commit

--- backend/src/core/engine/index.js
+++ backend/src/core/engine/index.js
@@ -1,3 +1,3 @@
 export { createApp } from './web/app.js';
-export { startServer } from './web/server.js';
 export { connectDB } from './db/connect.js';
+export { startServer } from './web/server.js';
