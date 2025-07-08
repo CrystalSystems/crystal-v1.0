d06ff83ba19c940b4f959f3897fa55ff82cc971f
Andrew <andrew.shedov@gmail.com>
2025-07-08 22:59:57 +0300

    commit

---
+++ frontend/src/shared/ui/index.js
@@ -0,0 +1,6 @@
+export * from "./SVG";
+export { Search } from "./Search";
+export { CookiesBanner } from "./CookiesBanner";
+export { UpButton } from "./UpButton";
+export { LoadingBar } from "./LoadingBar";
+export { Loader } from "./Loader";
